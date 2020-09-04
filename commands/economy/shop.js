49994cd7c95a39637b6e087a3486672df2845ba1
hola-sebas <danielpalma2003@hotmail.com>
2020-09-03 19:01:38 -0500

    first commit

---
+++ commands/economy/shop.js
@@ -0,0 +1,33 @@
+const db = require('megadb')
+module.exports = {
+    name: 'shop',
+    description: 'Compra, vende y revisa la tienda de otros jugadores',
+    usage: 'shop < buy / shell / show >',
+    permissions: ['SEND_MESSAGES', 'VIEW_CHANNEL', 'EMBED_LINKS'],
+    category: __dirname.split('\\').pop(),
+    disable: true,
+    execute: async (message, args) => {
+        const arg1 = args[0]
+        if (!arg1) return message.channel.send('Debes especificar una accion para realizar asi \`buy\` \`shell\` \`show\`')
+        switch (arg1) {
+            case 'buy':
+                message.channel.send('En proceso....')
+                break;
+            case 'shell':
+                let arg2 = args[1]
+                if (!arg2.length) return message.channel.send('Debes especificar el item que quieres vender')
+                const config = new db.crearDB(message.author.id, 'usuarios')
+                let bagDB = await config.get('inventory.bag')
+                let bag = bagDB.find(item => item.item == arg2)
+                if (!bag) return message.channel.send('hmm al parecer no tienes ese objeto en tu mochila')
+                message.channel.send(bag)
+                break;
+            case 'show':
+                message.channel.send('En proceso....')
+                break;
+            default:
+                message.channel.send('Esa accion no existe\nDebes especificar una accion para realizar asi \`buy\` \`shell\` \`show\`')
+                break;
+        }
+    }
+}
